3d016968a57f3803b4728c84bfa42e9d1a6088f5
NadiaArroyo25 <niac2596@gmail.com>
2025-01-15 13:47:41 -0600

    hashie

--- lib/gistance/connection.rb
+++ lib/gistance/connection.rb
@@ -1,4 +1,5 @@
 require 'faraday'
+require 'hashie'
 
 module Gistance
   # Faraday connection methods
@@ -14,7 +15,10 @@ module Gistance
       connection = Faraday.new(options) do |conn|
 
         conn.request :json
-        conn.response :json, content_type: /\bjson$/
+        conn.response :json, content_type: /\bjson$/ do |env|
+
+          env.body = Hashie::Mash.new(env.body) if env.body.is_a?(Hash)
+        end
 
         conn.use Gistance::Response::RaiseError
         conn.use FaradayMiddleware::FollowRedirects, limit: 3
